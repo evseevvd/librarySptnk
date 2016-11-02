df43b68dbe1870f1dce139653a441e543068fddf
evseevvd <evseevvd@sib-soft.ru>
2016-11-02 16:43:06 +0700

    рест допилил.

--- web/rest/sptnk-rest/src/main/java/co/sptnk/rest/LibServiceRest.java
+++ web/rest/sptnk-rest/src/main/java/co/sptnk/rest/LibServiceRest.java
@@ -2,6 +2,8 @@ package co.sptnk.rest;
 
 import co.sptnk.service.api.LibraryService;
 import co.sptnk.service.api.dto.BookDto;
+import co.sptnk.service.api.dto.criteria.SearchCriteriaBook;
+import co.sptnk.service.api.dto.response.AddOrUpdateResponse;
 import co.sptnk.service.api.dto.response.FindResponse;
 
 import javax.ejb.EJB;
@@ -24,7 +26,7 @@ import javax.ws.rs.core.Response;
 @TransactionAttribute(TransactionAttributeType.NEVER)
 public class LibServiceRest {
 
-    @EJB(lookup = "ejb:spt-service-ear/spt-service-impl/LibraryServiceImpl!co.sptnk.service.api.LibraryService")
+    @EJB(lookup = "ejb:spt-service-ear/spt-service-impl/LibraryService!co.sptnk.service.api.LibraryService")
     private LibraryService libraryService;
 
     @GET
@@ -36,7 +38,29 @@ public class LibServiceRest {
     @POST
     @Path("/add")
     public Response createBook(BookDto dto) {
-        return Response.ok().build();
+        try {
+            AddOrUpdateResponse response = libraryService.addOrUpdateBook(dto);
+            return Response.ok().entity(response).build();
+        } catch (IllegalAccessException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        } catch (InstantiationException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Path("/remove/{id}")
+    public Response removeBook(@PathParam("id") String id) {
+        BookDto bookDto = new BookDto();
+        bookDto.setId(id);
+        try {
+            libraryService.deleteBook(bookDto);
+            return Response.ok().build();
+        } catch (IllegalAccessException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        } catch (InstantiationException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        }
     }
 
     @GET
@@ -51,6 +75,19 @@ public class LibServiceRest {
             return Response.serverError().entity(e.getMessage()).build();
         } catch (NotFoundException e) {
             return Response.status(404).entity(e.getMessage()).build();
+        }
+    }
+
+    @POST
+    @Path("/search")
+    public Response searchBooks (SearchCriteriaBook criteria) {
+        try {
+            FindResponse response = libraryService.findBooks(criteria);
+            return Response.ok().entity(response).build();
+        } catch (IllegalAccessException e) {
+            return Response.serverError().entity(e.getMessage()).build();
+        } catch (InstantiationException e) {
+            return Response.serverError().entity(e.getMessage()).build();
         }
     }
 }
